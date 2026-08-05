bfeaf9a997c6c16194fec849c2b6371470106f45
ghostmind2026 <kdultimate2013@gmail.com>
2026-08-05 23:26:53 +0300

    Prepare Android activity for biometric authentication

--- android/app/src/main/kotlin/com/neizami/neizami_operations/MainActivity.kt
+++ android/app/src/main/kotlin/com/neizami/neizami_operations/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.neizami.neizami_operations
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
