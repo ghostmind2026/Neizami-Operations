4fd8dc24701a0aa254487a758ba737a0154c0620
ghostmind2026 <kdultimate2013@gmail.com>
2026-08-05 22:15:17 +0300

    feat: add runtime app configuration

---
+++ lib/config/app_config.dart
@@ -0,0 +1,20 @@
+class AppConfig {
+  static const String baseUrl = String.fromEnvironment(
+    'NEIZAMI_BASE_URL',
+    defaultValue: 'https://signalsjo.com',
+  );
+
+  static const String appId = String.fromEnvironment(
+    'NEIZAMI_APP_ID',
+    defaultValue: 'operations',
+  );
+
+  static const String apiPrefix = '/wp-json/neizami-mobile/v1';
+
+  static Uri uri(String path, [Map<String, dynamic>? query]) {
+    final cleanBase = baseUrl.replaceAll(RegExp(r'/+$'), '');
+    return Uri.parse('$cleanBase$apiPrefix$path').replace(
+      queryParameters: query?.map((key, value) => MapEntry(key, '$value')),
+    );
+  }
+}
